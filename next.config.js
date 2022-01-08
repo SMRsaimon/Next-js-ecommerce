7bb2a2d85b74c3a6ce85b7913cab14256942c4f6
SMRsaimon <smrsaimon960@gmail.com>
2022-01-08 04:21:44 -0800

    first commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    env: {
+        "BASE_URL": "http://localhost:3000",
+        "MONGODB_URL": "mongodb://localhost:27017",
+        "ACCESS_TOKEN_SECRET": "YOUR_ACCESS_TOKEN_SECRET",
+        "REFRESH_TOKEN_SECRET": "YOUR_REFRESH_TOKEN_SECRET",
+        "PAYPAL_CLIENT_ID": "YOUR_PAYPAL_CLIENT_ID",
+        "CLOUD_UPDATE_PRESET": "YOUR_CLOUD_UPDATE_PRESET",
+        "CLOUD_NAME": "smrsaimon960@gmail.com",
+        "CLOUD_API": "client_114790d0a99548e4ae9e0b6"
+    },
+    trailingSlash: true
+}
